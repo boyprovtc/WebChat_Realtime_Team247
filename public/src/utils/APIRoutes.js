71b9a82c42c5e868cb2d35bdc6432fb9110ef74b
MinhTri <phambachminhtri9.12@gmail.com>
2023-03-04 15:44:23 +0700

    fix again

---
+++ public/src/utils/APIRoutes.js
@@ -0,0 +1,6 @@
+const host = "http://localhost:5000";
+export const registerRoute = `${host}/api/auth/register`;
+export const loginRoute = `${host}/api/auth/login`;
+export const setAvatatRoute = `${host}/api/auth/setAvatar`;
+export const allUserRoute = `${host}/api/auth/allusers`;
+
--- server/controller/usersController.js
+++ server/controller/usersController.js
@@ -4,13 +4,13 @@ const bcrypt = require("bcrypt");
 
 module.exports.register = async (req, res, next) => {
     try {
-        const{username, email, password} = req.body;
-        const usernameCheck = await User.findOne({username});
+        const { username, email, password } = req.body;
+        const usernameCheck = await User.findOne({ username });
         if (usernameCheck)
-            return res.json({msg: "Username used", status: false});
-        const emailCheck = await User.findOne({email});
+            return res.json({ msg: "Username used", status: false });
+        const emailCheck = await User.findOne({ email });
         if (emailCheck)
-            return res.json({msg: "Email used", status: false});
+            return res.json({ msg: "Email used", status: false });
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = await User.create({
             email,
@@ -18,7 +18,7 @@ module.exports.register = async (req, res, next) => {
             password: hashedPassword,
         });
         delete user.password;
-        return res.json({status: true, user});
+        return res.json({ status: true, user });
     } catch (ex) {
         next(ex);
     }
@@ -26,15 +26,15 @@ module.exports.register = async (req, res, next) => {
 
 module.exports.login = async (req, res, next) => {
     try {
-        const{username, password} = req.body;
-        const user = await User.findOne({username});
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
         if (!user)
-            return res.json({msg: "Incorrect username or password", status: false});
+            return res.json({ msg: "Incorrect username or password", status: false });
         const isPasswordValid = await bcrypt.compare(password, user.password);
         if (!isPasswordValid)
-            return res.json({msg: "Incorrect username or password", status: false});
+            return res.json({ msg: "Incorrect username or password", status: false });
         delete user.password;
-        return res.json({status: true, user});
+        return res.json({ status: true, user });
     } catch (ex) {
         next(ex);
     }
@@ -43,16 +43,31 @@ module.exports.login = async (req, res, next) => {
 module.exports.setAvatar = async (req, res, next) => {
     try {
         const userId = req.params.id;
-        const avatarImage =req.body.image;
-        const userData = await User.findByIdAndUpdate(userId,{
+        const avatarImage = req.body.image;
+        const userData = await User.findByIdAndUpdate(userId, {
             isAvatarImageSet: true,
             avatarImage,
         });
-       
+
         return res.json({
-            isSet: userData.isAvatarImageSet, 
-            image: userData.avatarImage
+            isSet: userData.isAvatarImageSet,
+            image: userData.avatarImage,
         });
+    } catch (ex) {
+        next(ex);
+    }
+};
+
+
+module.exports.getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.find({ _id: { $ne: req.params.id } }).select([
+            "email",
+            "username",
+            "avatarImage",
+            "_id",
+        ]);
+        return res.json(users);
     } catch (ex) {
         next(ex);
     }
